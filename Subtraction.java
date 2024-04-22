1903c11943991e7949f07464e096018d58c81cb4
harshapriya <harshapriyathiruveedhi1973@gmail.com>
2024-04-22 18:11:55 +0530

    added files

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        // Assign the given numbers to variables
+        double num1 = 10.5;
+        double num2 = 5.3;
+        
+        // Calculate the result of subtraction
+        double result = num1 - num2;
+        System.out.println("Hello, harsha!");
+
+        // Print the result
+        System.out.println("Result of subtraction: " + result);
+
+    }
+}
